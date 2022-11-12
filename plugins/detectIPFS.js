b2304b70086eb3896f9ccb8bf37a99a08268854f
gozzy <samuel.jj.gosling@gmail.com>
2022-11-12 01:12:11 +0000

    improve ipfs detection & update notifications

--- plugins/detectIPFS.js
+++ plugins/detectIPFS.js
@@ -3,13 +3,28 @@ export default ({ store, isHMR, app }, inject) => {
   inject('isLoadedFromIPFS', main)
 }
 function main() {
-  const whiteListedDomains = ['localhost:3000', 'tornadocash.eth.link', 'tornadocash.eth.limo']
+  const whiteListedDomains = [
+    'tornadocash.3th.li',
+    'tornadocash.3th.ws',
+    'tornadocash.eth.link',
+    'tornadocash.eth.limo',
+    'tornadocashcommunity.3th.li',
+    'tornadocashcommunity.3th.ws',
+    'tornadocashcommunity.eth.link',
+    'tornadocashcommunity.eth.limo'
+  ]
 
-  const NETLIFY_REGEXP = /deploy-preview-(\d+)--tornadocash\.netlify\.app/
+  const IPFS_GATEWAY_REGEXP = /.ipfs./
+  const IPFS_LOCAL_REGEXP = /.ipfs.localhost:/
+  const IPFS_SOP_GATEWAY_REGEXP = /\/ipfs\//
 
-  if (NETLIFY_REGEXP.test(window.location.host)) {
+  if (IPFS_LOCAL_REGEXP.test(window.location.host)) {
     return false
-  } else if (!whiteListedDomains.includes(window.location.host)) {
+  } else if (
+    IPFS_GATEWAY_REGEXP.test(window.location.host) ||
+    IPFS_SOP_GATEWAY_REGEXP.test(window.location.host) ||
+    whiteListedDomains.includes(window.location.host)
+  ) {
     console.warn('The page has been loaded from ipfs.io. LocalStorage is disabled')
     return true
   }
